c408207a899606b1e0d62f3f3e5112e1955187ea
phyeonbin01 <phyeonbin01@gmail.com>
2023-06-11 13:22:36 +0900

    주석 실수 처리

--- useWhisper/urls.py
+++ useWhisper/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import upload_audio, result_page
-
+from .views import upload_audio
+#from .views import result_page
 urlpatterns = [
     path('uploaded/', upload_audio.as_view(), name='Audio uploaded'),
     #get response 들어올 시 경로 설정
